14ac6ae6129b1d1d1a21422b8118af718587bbee
julgachancipa <julianaga01@gmail.com>
2019-11-08 21:04:41 +0000

    [Feature]: create v.2 of the base model close #5

--- models/base_model.py
+++ models/base_model.py
@@ -7,13 +7,26 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         init method
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == 'updated_at':
+                    self.updated_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == 'my_number':
+                    self.my_number = value
+                if key == 'name':
+                    self.name = value
+                if key == 'id':
+                    self.id = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
