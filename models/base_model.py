8876a4ed9ee9a358cb7aff820c6e858bcf219986
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-11-09 01:05:12 +0000

    [Base]: update some files for task 5

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,12 @@ class BaseModel:
         """
         init method
         """
-        if kwargs is not None and len(kwargs) > 0:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
             for key, value in kwargs.items():
                 if key == 'created_at':
                     self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
@@ -24,12 +29,6 @@ class BaseModel:
                     self.name = value
                 if key == 'id':
                     self.id = value
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            print(models)
-            models.storage
 
     def __str__(self):
         """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,7 +7,7 @@ import json
 import os.path
 from models.base_model import BaseModel
 
-class FileStorage(BaseModel):
+class FileStorage():
     """
     Class for file storage
     """
@@ -24,25 +24,28 @@ class FileStorage(BaseModel):
         """
         Set objects
         """
-        print(type(obj))
-        print("Este es ", obj)
+        key = str(obj.__class__.__name__) + '.' + str(obj.id)
+        value = obj.to_dict()
+        self.__objects[key] = value
+        print(self.__objects)
 
-        self.__objects[obj['__class__'] + '.' + obj['id']] = obj
 
     def save(self):
         """
         Save the serializes of objects in JSON file
         """
-        with open(self.__file_path, 'w') as f:
-            dict1 = self.to_dict()
-            dict1 = json.dumps(dict1)
-            f.write(dict1)
+        dict1 = {}
+        for key, val in self.__objects.items():
+            dict1[key] = str(val)
+
+        print(dict1)
+        with open(self.__file_path, 'w', encoding="UTF8") as f:
+            json.dump(dict1, f)
 
     def reload(self):
         """
         Reload the deserializes the JSON file
         """
         if os.path.isfile(self.__file_path):
-            with open(self.__file_path, 'r') as f:
-                data = f.read()
-                print(data)
+            with open(self.__file_path, 'r', encoding="UTF8") as f:
+                __objects = json.load(f)
