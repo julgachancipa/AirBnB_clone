852ab43b0bf54ab52da9da8df038c9326438b07a
julgachancipa <julianaga01@gmail.com>
2019-11-09 23:47:43 -0500

    [UPDATE] - v3 missing correct output of the ids

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Base Model
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -11,7 +12,12 @@ class BaseModel:
         """
         init method
         """
-        if kwargs is not None and len(kwargs) > 0:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
             for key, value in kwargs.items():
                 if key == 'created_at':
                     self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
@@ -23,10 +29,6 @@ class BaseModel:
                     self.name = value
                 if key == 'id':
                     self.id = value
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -37,9 +39,11 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        self.__dict__['__class__'] = self.__class__.__name__
-        self.__dict__['created_at'] = self.created_at.isoformat()
-        self.__dict__['updated_at'] = self.updated_at.isoformat()
-        return (self.__dict__)
+        cpy_dict = dict(self.__dict__)
+        cpy_dict['__class__'] = self.__class__.__name__
+        cpy_dict['created_at'] = self.created_at.isoformat()
+        cpy_dict['updated_at'] = self.updated_at.isoformat()
+        return (cpy_dict)
