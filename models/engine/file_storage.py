f6cd20cec99278b6c8bddc934600086f4ae5b32e
Juan Sebastian Calle <jusemonca@gmail.com>
2019-11-11 20:32:31 -0500

    [Feature]: update file for manage the User class closed #11

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,8 @@ File storage
 """
 import json
 import os.path
+from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage:
@@ -37,12 +39,14 @@ class FileStorage:
             json.dump(dict1, f)
 
     def reload(self):
-        from models.base_model import BaseModel
         """
         Reload the deserializes the JSON file
         """
+        classes = {'BaseModel': BaseModel, 'User': User}
         if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, encoding="UTF8") as f:
                 super_dict = json.load(f)
             for key, value in super_dict.items():
-                FileStorage.__objects[key] = BaseModel(**value)
+                key_class = value['__class__']
+                obj = classes.get(key_class)
+                FileStorage.__objects[key] = obj(**value)
