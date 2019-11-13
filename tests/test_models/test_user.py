329573d5d5b4f35f34a52f2c4f57d4ef2590ddc5
julgachancipa <julianaga01@gmail.com>
2019-11-13 20:53:35 +0000

    [Feauture] - UNITTESTS V6

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -18,9 +18,17 @@ class TestBaseModelClass(unittest.TestCase):
         my_model = User()
         my_model.name = "Holbi"
         my_model.my_number = 89
+        my_model.first_name = "Juli"
+        my_model.last_name = "Gacast"
+        my_model.email = "jul@ga.com"
+        my_model.password = "root"
         self.assertEqual(my_model.name, "Holbi")
         self.assertEqual(my_model.my_number, 89)
         self.assertEqual(my_model.__class__.__name__, "User")
+        self.assertEqual(my_model.first_name, "Juli")
+        self.assertEqual(my_model.last_name, "Gacast")
+        self.assertEqual(my_model.email, "jul@ga.com")
+        self.assertEqual(my_model.password, "root")
         self.assertTrue(isinstance(my_model.created_at, datetime))
         self.assertTrue(isinstance(my_model.updated_at, datetime))
         my_model.save()
